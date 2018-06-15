132bf2fa0c97e81274368409113d5dd5a3bc6917
Dominik Handler <dominik.handler@imba.oeaw.ac.at>
2018-06-15 09:11:18 +0200

    completely new R installation - works now with X11

--- Singularity.R
+++ Singularity.R
@@ -1,8 +1,6 @@
 #R in singularity
-
-BootStrap: debootstrap
-OSVersion: xenial
-MirrorURL: http://us.archive.ubuntu.com/ubuntu/
+Bootstrap: docker
+From: ubuntu:16.04
 
 %runscript
   "$@"
@@ -10,10 +8,7 @@ MirrorURL: http://us.archive.ubuntu.com/ubuntu/
 %post
 
   apt-get update
-  apt-get -y install software-properties-common build-essential wget git binutils binutils-dev cmake gcc g++ gfortran bzip2  xz-utils liblzma-dev make libcurl4-openssl-dev libreadline-dev libpcre3-dev libbz2-dev zlib1g-dev libssl-dev libxml2-dev xauth xorg-dev libcairo2-dev libxt-dev 
-  add-apt-repository universe
-  apt-get update
-  apt-get -y install pandoc
+  apt-get -y install wget git sudo curl libssl-dev 
   
   apt-get clean && apt-get update && apt-get install -y \
       locales \
@@ -25,25 +20,32 @@ MirrorURL: http://us.archive.ubuntu.com/ubuntu/
       locale-gen en_US.UTF-8 && \
       dpkg-reconfigure locales
 
-  mkdir /R/
-  cd /R/
-  wget https://cran.r-project.org/src/base/R-latest.tar.gz
-  tar zxf R-latest.tar.gz
-  rm -rf R-latest.tar.gz
-  cd R-*
-  ./configure --enable-R-static-lib --with-blas --with-lapack --enable-R-shlib=yes
-
+  #insall R-dependencies
+  apt-get -y build-dep r-base
+  
+  #install R
+  mkdir /R
+  cd /R
+  version="3.5.0"
+  wget http://cran.at.r-project.org/src/base/R-3/R-$version.tar.gz
+  tar -xzf R-$version.tar.gz  
+  cd /R/R-$version
+  ./configure 
   make
   make install
 
-  apt-get update
+  apt-get clean
+
+  #install R-packages  
   R --slave -e 'install.packages(c( "devtools"), repos = "http://cran.wu.ac.at/") '
   R --slave -e 'install.packages(c( "tidyverse", "plotly", "Cairo"), repos = "http://cran.wu.ac.at/") '
   R --slave -e 'options(unzip = "internal"); devtools::install_github("tidyverse/ggplot2") '
   R --slave -e 'install.packages(c( "cowplot" ), repos = "http://cran.wu.ac.at/") '
   R --slave -e 'source("http://bioconductor.org/biocLite.R"); biocLite("remotes"); biocLite("pachterlab/sleuth@v0.30.0")'
   R --slave -e 'source("http://bioconductor.org/biocLite.R"); biocLite("COMBINE-lab/wasabi")'
-  
+
+  apt-get clean
+    
   mkdir /groups
   mkdir /scratch
   mkdir /scratch-ii2
